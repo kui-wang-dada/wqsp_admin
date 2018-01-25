632735a4528a80f41cd079d3a3ada9f9538d52b1
gaos <714851002@qq.com>
2018-01-25 21:18:38 +0800

    初始版本

---
+++ src/const/api.js
@@ -0,0 +1,3 @@
+// const baseUrl = 'https://papitest.yunlitech.com/api/v1/'
+
+// export default getUser = baseUrl + 'user/info'
---
+++ src/server/userServer.js
@@ -0,0 +1,8 @@
+// // import http from ''
+// import getUser from '../const/api.js'
+
+// export default userServer = {
+//   getUser() {
+//     return getUser
+//   }
+// }
