6f169e4de0cc16b036963aeb3f5a28457e6bdb67
gaos <714851002@qq.com>
2018-01-31 14:12:41 +0800

    初始版本

--- build/build.js
+++ build/build.js
@@ -1,21 +1,21 @@
+'use strict'
 require('./check-versions')()
 
-process.env.NODE_ENV = 'production'
+const ora = require('ora')
+const rm = require('rimraf')
+const path = require('path')
+const chalk = require('chalk')
+const webpack = require('webpack')
+const config = require('../config')
+const webpackConfig = require('./webpack.prod.conf')
+const server = require('pushstate-server')
 
-var ora = require('ora')
-var rm = require('rimraf')
-var path = require('path')
-var chalk = require('chalk')
-var webpack = require('webpack')
-var config = require('../config')
-var webpackConfig = require('./webpack.prod.conf')
-
-var spinner = ora('building for production...')
+var spinner = ora('building for '+ process.env.env_config+ ' environment...' )
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
-  webpack(webpackConfig, function (err, stats) {
+  webpack(webpackConfig, (err, stats) => {
     spinner.stop()
     if (err) throw err
     process.stdout.write(stats.toString({
@@ -26,10 +26,23 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       chunkModules: false
     }) + '\n\n')
 
+    if (stats.hasErrors()) {
+      console.log(chalk.red('  Build failed with errors.\n'))
+      process.exit(1)
+    }
+
     console.log(chalk.cyan('  Build complete.\n'))
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
     ))
+    if(process.env.npm_config_preview){
+      server.start({
+          port: 9526,
+          directory: './dist',
+          file: '/index.html'
+      });
+      console.log('> Listening at ' +  'http://localhost:9526' + '\n')
+    }
   })
 })
--- src/main.js
+++ src/main.js
@@ -1,52 +1,41 @@
-import babelpolyfill from 'babel-polyfill'
 import Vue from 'vue'
+
+import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
+
 import App from './App'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-//import './assets/theme/theme-green/index.css'
-import VueRouter from 'vue-router'
-import store from './vuex/store'
-import Vuex from 'vuex'
-//import NProgress from 'nprogress'
-//import 'nprogress/nprogress.css'
-import routes from './routes'
-import Mock from './mock'
-Mock.bootstrap();
-import 'font-awesome/css/font-awesome.min.css'
+import router from './router'
+import store from './store'
 
-Vue.use(ElementUI)
-Vue.use(VueRouter)
-Vue.use(Vuex)
+import i18n from './lang' // Internationalization
+import './icons' // icon
+import './errorLog'// error log
+import './permission' // permission control
+import './mock' // simulation data
 
-//NProgress.configure({ showSpinner: false });
+import * as filters from './filters' // global filters
 
-const router = new VueRouter({
-  routes
+Vue.use(Element, {
+  size: 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
 })
 
-router.beforeEach((to, from, next) => {
-  //NProgress.start();
-  if (to.path == '/login') {
-    sessionStorage.removeItem('user');
-  }
-  let user = JSON.parse(sessionStorage.getItem('user'));
-  if (!user && to.path != '/login') {
-    next({ path: '/login' })
-  } else {
-    next()
-  }
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
 })
 
-//router.afterEach(transition => {
-//NProgress.done();
-//});
+Vue.config.productionTip = false
 
 new Vue({
-  //el: '#app',
-  //template: '<App/>',
+  el: '#app',
   router,
   store,
-  //components: { App }
-  render: h => h(App)
-}).$mount('#app')
-
+  i18n,
+  template: '<App/>',
+  components: { App }
+})
